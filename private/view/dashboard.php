e9bb630ab61ee766fecce5161b902eda163c1db2
djenidisimple <djenidigauss@gmail.com>
2025-04-25 22:17:29 +0300

    ok installation finie

---
+++ private/view/dashboard.php
@@ -0,0 +1,5 @@
+<?php
+    include "../base/header.php";
+    echo "<h1>Bienvenu Admin!</h1>";
+    include "../base/fooster.php";
+?>
---
+++ public/view/home.php
@@ -0,0 +1,5 @@
+<?php
+    include "../base/header.php";
+    echo "<h1>Bienvenu dans chers client!</h1>";
+    include "../base/fooster.php";
+?>
